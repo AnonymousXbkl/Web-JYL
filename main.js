c75b6f208dc91c2f8b382e249827a3c44a720049
AnonymousXbkl <kerjaandi01@gmail.com>
2024-03-02 12:42:41 +0700

    Melakukan perubahan pada semua responsif device, baru ditambahkan lagi responsif under width 600px

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 const searchButton = document.getElementById ('search-button');
 const searchForm = document.querySelector ('.search-form');
-const searchBox = document.getElementById ('search-box')
+const searchBox = document.getElementById ('search-box');
+const inputSearch = document.querySelector ('.input-search')
     searchButton.addEventListener ('click', function (e) {
         searchForm.classList.toggle ('active');
-        searchBox.focus ();
+        inputSearch.focus ();
+        e.preventDefault ();
     });
